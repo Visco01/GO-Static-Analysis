c479db0ce742f10c5e211b321c7d6210e85e4472
Visco01 <visconti373@gmail.com>
2024-07-24 11:04:18 +0200

    Fix report generation logic

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
+# FLOW: For each model, for each algorithm, for each file, analyze with each analysis.
 SIGN='sign'
 PARITY='parity'
-analyses=($PARITY $SIGN)
+INTERVALS='intervals'
+TAINT='taint'
+PENTAGONS='pentagons'
+PREFIX='prefix'
+SUFFIX='suffix'
+TARSIS='tarsis'
+analyses=($PARITY $SIGN $INTERVALS $TAINT $PENTAGONS $PREFIX $SUFFIX $TARSIS)
 
 ARRAY_MANIPULATION='/array-manipulation'
 CUSTOM='/custom'
@@ -17,17 +24,27 @@ GO_LISA='/Users/visco/Documents/code/GO-Static-Analysis/go-lisa/go-lisa-0.1/bin/
 current_directory=$(pwd)
 file_count=0
 
-for analysis in "${analyses[@]}"; do
-    echo "Analyzing with $analysis"
-    for model in "${models[@]}"; do
-        echo "Analyzing with $model as model."
-        for algorithm in "${algorithms[@]}"; do
-            echo "Analyzing with $algorithm as algorithm."
-            cd "$model/$algorithm"
-            file_list=($(find . -type f))
+
+echo "Analyzing with $analysis"
+for model in "${models[@]}"; do
+    echo "Analyzing with $model as model."
+    for algorithm in "${algorithms[@]}"; do
+        echo "Analyzing with $algorithm as algorithm."
+        cd "$model/$algorithm"
+        file_list=($(find . -type f))
+        if [ $algorithm == $ARRAY_MANIPULATION ]; then
+            analyses=($PARITY $SIGN)
+        elif [ $algorithm == $CUSTOM ]; then
+            analyses=($PARITY $TARSIS)
+        elif [ $algorithm == $SORTING ]; then
+            analyses=($SUFFIX $TARSIS)
+        elif [ $algorithm == $STRING_MANIPULATION ]; then
+            analyses=($INTERVALS $TAINT)
+        fi
+        for analysis in "${analyses[@]}"; do
             for file in "${file_list[@]}"; do
                 echo "Analyzing $file"
-                "$GO_LISA" -i "$file" -o "$model/../output/$analysis/$algorithm" -a "$analysis"
+                "$GO_LISA" -i "$file" -o "$model/../output/$algorithm/$file/$analysis" -a "$analysis"
                 file_count=$((file_count + 1))
             done
         done
